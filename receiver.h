f2b7c8342d2cbaaec7f81fbbb398fefa0f0d7820
Sean Karlage <sean.karlage@gmail.com>
2015-04-26 14:32:40 -0400

    removing old files that are no longer used in project

--- receiver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Passing char * so port can get passed to getaddrinfo correctly
-_Noreturn void run_server(char *, char *);
-
-// Signal handler functions
-void init_signalhandler(void);
-void sigchld_handler(int sig);
